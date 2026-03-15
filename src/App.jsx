4bd080d3248dcdb1394bc994abc5e9a326c517c0
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2026-03-15 13:02:20 -0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ProfilePage from './pages/ProfilePage'
 import UploadPage from './pages/UploadPage'
 import SearchPage from './pages/SearchPage'
 import AdminPage from './pages/AdminPage'
+import MessagesPage from './pages/MessagesPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 // Redirige /settings → perfil propio
@@ -71,6 +72,22 @@ export default function App() {
               element={
                 <ProtectedRoute requireStaff>
                   <AdminPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/messages"
+              element={
+                <ProtectedRoute>
+                  <MessagesPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/messages/:convId"
+              element={
+                <ProtectedRoute>
+                  <MessagesPage />
                 </ProtectedRoute>
               }
             />
