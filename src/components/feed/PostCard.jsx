50116a72b8e22a12453581db5f11c71c8a14fdb3
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2026-03-16 10:03:12 -0600

    Update PostCard.jsx

--- src/components/feed/PostCard.jsx
+++ src/components/feed/PostCard.jsx
@@ -24,7 +24,7 @@ const CATS = {
 
 function getYouTubeId(url) {
   if (!url) return null
-  const m = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/)([^&\n?#]+)/)
+  const m = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/|youtube\.com\/shorts\/)([^&\n?#]+)/)
   return m ? m[1] : null
 }
 
