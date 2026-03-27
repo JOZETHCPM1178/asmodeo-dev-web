266f8a73ae9df293494d90420f1d7633458bab31
JOZETHCPM1178 <enmanuelalex490@gmail.com>
2026-03-26 22:13:11 -0600

    Add files via upload

--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -7,7 +7,7 @@ import { es } from 'date-fns/locale'
 import {
   getPost, toggleLike, hasLiked, registerDownload,
   reportPost, deletePost, toggleFeatured, verifyPost,
-  setPostStatus, updatePost,
+  setPostStatus, updatePost, getPostUrl,
 } from '../services/posts'
 import { useAuth } from '../context/AuthContext'
 import { optimizeUrl, uploadImage } from '../services/cloudinary'
